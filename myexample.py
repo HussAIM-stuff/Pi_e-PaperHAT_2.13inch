d8a3e7747195766041d192134b2823e490162566
Hussein  Abdeljabbar Ismail Martínez <hussein.odeh@gmail.com>
2020-10-08 20:18:27 +0300

    Update myexample.py

--- myexample.py
+++ myexample.py
@@ -23,14 +23,16 @@ epd.Clear(0xFF)
 font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
 font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 
-# # partial update
-logging.info("4.show time...")
-epd.init(epd.lut_partial_update)
-epd.Clear(0xFF)
 
 time_image = Image.new('1', (epd.height, epd.width), 255)
 time_draw = ImageDraw.Draw(time_image)
 time_draw.rectangle((120, 80, 220, 105), fill = 255)
-time_draw.text((12, 20), "IP: "+([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]), font = font24, fill = 0)
+import socker
+time_draw.text((5, 20), "local IP: "+([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]), font = font15, fill = 0)
+
+import urllib.request
+
+time_draw.text((5, 70),"public IP: "+urllib.request.urlopen('https://ident.me').read().decode('utf8'), font = font15, fill = 0)
+
 #    time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
 epd.display(epd.getbuffer(time_image))
